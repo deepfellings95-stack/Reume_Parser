7a44a6b6f2b03e7cbc2077a46ffd488729c2a97d
unknown <deepfellings95@gmail.com>
2026-01-13 13:11:44 +0530

    feat: deleting the unnecessary folders

--- migrate_database.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from database.database import db
-from flask_migrate import upgrade, Migrate
-
-migrate = Migrate(app, db)
-
-with app.app_context():
-    upgrade()  # applies all migrations
-    print("Database migrated successfully!")
